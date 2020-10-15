e3eaa1e2ccc690b54dd671604f3574295020b2e1
Takayuki Nagashima <hqf00342@nifty.com>
2020-10-15 12:06:33 +0900

    1st commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace MinimizeParentWindow
+{
+    internal static class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_MINIMIZE = 6;
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                //Get the parent PID.
+                var pc = new PerformanceCounter("Process", "Creating Process ID", Process.GetCurrentProcess().ProcessName);
+                var ppid = (int)pc.NextValue();
+                if (ppid <= 0)
+                    throw new Exception("Cannot get parent process ID.");
+
+                //get window handle of the parent process.
+                var hwnd = Process.GetProcessById(ppid).MainWindowHandle;
+                if (hwnd == IntPtr.Zero)
+                    throw new Exception("Cannot get parent window handle.");
+
+                //Minimize the parent window
+                ShowWindow(hwnd, SW_MINIMIZE);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.GetType().Name} : {e.Message}");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+﻿# MinimizeParentWindow
+
+Minimize the parent window, such as the command prompt.  
+
+This program is intended to be used in a batch file.  
+
+## How to use
+
+This program has no options, just run it in a .bat file.
+
+``` bat
+@echo off
+
+MinimizeParentWindow.exe
+...
+```
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+Copyright (c) 2020 Takayuki Nagashima <hqf00342@nifty.com>
